3e98f2c1c20088867eb38110c06b9d76becfc22f
Louis <louis.potterie@efrei.net>
2019-05-08 11:35:52 +0200

    new

---
+++ src/com/company/Driver.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.sql.*;
+
+public class Driver {
+    public static void main(String[] args) {
+        try {
+            //conn to DB
+
+
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/ecole?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC","root","");
+            //Create statement
+            Statement stmt = conn.createStatement();
+            //SQL Query
+            ResultSet res = stmt.executeQuery("select * from nom");
+            //Process result
+            while(res.next()) {
+                System.out.println(res.getString("A")+res.getString("B")+res.getString("C"));
+            }
+
+        }
+        catch (Exception exc){
+            exc.printStackTrace();
+
+        }
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.sql.*;
+
+public class Main {
+
+    public class connect {
+        static final String JDBC_DRIVER = "com.sql.jdbc.Driver";
+        static final String DB_URL = "jdbc:mysql://localhost/tp1";
+
+        static final String USER = "root";
+        static final String PASSWORD = "";
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+
